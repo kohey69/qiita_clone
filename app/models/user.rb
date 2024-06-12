abc964ba49ce92a781a201d238017a0805665e0e
kuma <kuma@sonicgarden.jp>
2024-06-12 11:42:39 +0900

    feat: ユーザー登録できるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :validatable
 
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
   validates :name, presence: true
 end
