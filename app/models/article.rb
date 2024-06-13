86eab3b26c538fef6e9acddc8c950951a1077a63
kuma <kuma@sonicgarden.jp>
2024-06-13 23:00:00 +0900

    refactor: 一時保存

--- app/models/article.rb
+++ app/models/article.rb
@@ -20,21 +20,20 @@ class Article < ApplicationRecord
   end
 
   def tags_must_be_less_than_or_equal_max_size
-    if tag_list.size > TAG_MAX_SIZE
+    if tags.size > TAG_MAX_SIZE
       errors.add(:tag_list, "は#{TAG_MAX_SIZE}個までしか登録できません。")
     end
   end
 
   def update_with_tags!(params)
-    transaction do
-      self.taggings.destroy_all
-      tag_names = params[:tag_list].split(',').map(&:strip)
-      tag_names.each do |tag_name|
-        tag = Tag.find_or_initialize_by(name: tag_name)
-        self.tags << tag
-      end
-      self.update!(params)
+    new_tags = []
+    tag_names = params[:tag_list].split(',').map(&:strip)
+    tag_names.each do |tag_name|
+      tag = Tag.find_or_create_by(name: tag_name)
+      new_tags << tag
     end
+    self.tag_ids = new_tags.pluck(:id)
+    self.update(params)
   end
 
   def self.build_with_tags(params)
