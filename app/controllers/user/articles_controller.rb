7de48ee6ad710280479093ace162cef59939beac
kuma <kuma@sonicgarden.jp>
2024-06-14 00:46:19 +0900

    feat: 同じ名前のタグを複数登録しようとするとdistinctしてタグが作成されるように

--- app/controllers/user/articles_controller.rb
+++ app/controllers/user/articles_controller.rb
@@ -18,7 +18,7 @@ class User::ArticlesController < User::ApplicationController
       redirect_to user_article_path(@article), notice: t('controllers.created')
     else
       flash.now[:alert] = t('controllers.failed')
-      render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_content
     end
   end
 
@@ -30,7 +30,7 @@ class User::ArticlesController < User::ApplicationController
       redirect_to user_article_path(@article), notice: t('controllers.updated')
     else
       flash.now[:alert] = t('controllers.failed')
-      render :edit, status: :unprocessable_entity
+      render :edit, status: :unprocessable_content
     end
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -9,7 +9,6 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validate :tags_must_be_less_than_or_equal_max_size
-  validate :tags_must_be_unique
 
   scope :default_order, -> { order(created_at: :desc) }
   scope :published, -> { where(published: true) }
@@ -22,21 +21,15 @@ class Article < ApplicationRecord
     new_tags = []
     tag_names = value.split(',').map(&:strip)
     tag_names.each do |tag_name|
-      tag = Tag.find_or_create_by!(name: tag_name)
+      tag = Tag.find_or_create_by(name: tag_name)
       new_tags << tag
     end
-    self.tag_ids = new_tags.pluck(:id)
+    self.tag_ids = new_tags.pluck(:id).uniq # tag_idsでtagging.rbのunique validationに引っかかり例外を吐くのでuniq
   end
 
   def tags_must_be_less_than_or_equal_max_size
     if tags.size > TAG_MAX_SIZE
       errors.add(:tag_list, "は#{TAG_MAX_SIZE}個までしか登録できません")
-    end
-  end
-
-  def tags_must_be_unique
-    if tags.uniq.length != tags.length
-      errors.add(:tag_list, 'は同じ名前のものは登録できません')
     end
   end
 end
