532f3741942205addb87ea92c4a40102bcf1139d
kuma <kuma@sonicgarden.jp>
2024-06-14 15:58:19 +0900

    feat: with_published_articlesのテスト作成

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Tag, type: :model do
       create(:tag, name: '記事に紐づいていないタグ')
       create(:article, :with_user, tags: [tag])
 
-      expect(Tag.with_published_articles).to contain_exactly(tag)
+      expect(Tag.with_published_articles.pluck(:name)).to contain_exactly('記事に紐づいているタグ')
     end
   end
 end
