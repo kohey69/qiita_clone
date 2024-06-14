0871129a23ed5296fef365da99e5bebf362edb96
kuma <kuma@sonicgarden.jp>
2024-06-14 10:48:18 +0900

    feat: 記事の本文についてmarkdown処理されて表示できるように

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,36 @@ module ApplicationHelper
       article_path(article)
     end
   end
+
+  def markdown(text)
+    markdown_options = {
+      hard_wrap: true,
+      link_attributes: { rel: 'nofollow', taget: '_blank' },
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      tables: true,
+      lax_spacing: true,
+      underline: true,
+      highlight: true,
+      quote: true,
+      footnotes: true,
+    }
+    html_options = {
+      filter_html: false,
+      no_images: false,
+      no_links: false,
+      no_styles: true,
+      escape_html: false,
+      safe_links_only: true,
+      with_toc_data: true,
+      hard_wrap: true,
+      xhtml: true,
+      prettify: true, # google-code-prettifyは利用しないが、スタイルと今後のために
+      link_attributes: { rel: 'nofollow', taget: '_blank' },
+    }
+    renderer = Redcarpet::Render::HTML.new(html_options)
+    markdown = Redcarpet::Markdown.new(renderer, markdown_options)
+    sanitize(markdown.render(text))
+  end
 end
