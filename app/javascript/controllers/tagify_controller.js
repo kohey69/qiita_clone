3b4cc3544732360f5af81e3fa5e2c2a6f0dcb43e
kuma <kuma@sonicgarden.jp>
2024-06-14 09:11:03 +0900

    refactor: 不要なTarget削除

--- app/javascript/controllers/tagify_controller.js
+++ app/javascript/controllers/tagify_controller.js
@@ -3,7 +3,7 @@ import Tagify from '@yaireo/tagify'
 
 // Connects to data-controller="tagify"
 export default class extends Controller {
-  static targets = [ "tagify", "tagNames" ]
+  static targets = [ "tagify" ]
   connect() {
     this.tagify = new Tagify(this.tagifyTarget, {
       maxTags: 5,
