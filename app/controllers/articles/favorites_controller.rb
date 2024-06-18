60ed4c043150befcd694bcfb8bfa94208ea08507
kuma <kuma@sonicgarden.jp>
2024-06-18 13:28:09 +0900

    refactor: viewsに表示しないインスタンス変数削除

--- app/controllers/articles/favorites_controller.rb
+++ app/controllers/articles/favorites_controller.rb
@@ -2,11 +2,11 @@ class Articles::FavoritesController < Articles::ApplicationController
   before_action :authenticate_user!
 
   def create
-    @favorite = current_user.favorites.find_or_create_by!(article: @article) # いいね済みの場合は例外を吐かないように
+    current_user.favorites.find_or_create_by!(article: @article) # いいね済みの場合は例外を吐かないように
   end
 
   def destroy
-    @favorite = current_user.favorites.find_by(article: @article)
-    @favorite&.destroy # windowを複数開いていいねを解除した時に例外を吐かないように
+    favorite = current_user.favorites.find_by(article: @article)
+    favorite&.destroy # windowを複数開いていいねを解除した時に例外を吐かないように
   end
 end
