9a5e35f87b20402d283f0e655f1838534e6fd730
kuma <kuma@sonicgarden.jp>
2024-06-18 13:25:10 +0900

    fix: typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @articles = @user.articles.published.default_order.page(params[:page])
-    @favorite_articles = @user.favorite_articles.published.default_order.page(params[:favorites_age])
+    @favorite_articles = @user.favorite_articles.published.default_order.page(params[:favorites_page])
   end
 end
