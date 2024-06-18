a5b261b90421120ab4508a640c95d2609b60d38d
kuma <kuma@sonicgarden.jp>
2024-06-18 10:47:25 +0900

    refactor: before_actionを共通化

--- app/controllers/articles/favorites_controller.rb
+++ app/controllers/articles/favorites_controller.rb
@@ -1,6 +1,5 @@
-class Articles::FavoritesController < ApplicationController
+class Articles::FavoritesController < Articles::ApplicationController
   before_action :authenticate_user!
-  before_action :set_article
 
   def create
     @favorite = current_user.favorites.find_or_create_by!(article: @article) # いいね済みの場合は例外を吐かないように
@@ -9,11 +8,5 @@ class Articles::FavoritesController < ApplicationController
   def destroy
     @favorite = current_user.favorites.find_by(article: @article)
     @favorite&.destroy # windowを複数開いていいねを解除した時に例外を吐かないように
-  end
-
-  private
-
-  def set_article
-    @article = Article.published.find(params[:article_id])
   end
 end
