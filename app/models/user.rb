7881714555f95dd0eff4ea0b1d262241d59f4c91
kuma <kuma@sonicgarden.jp>
2024-06-18 15:28:51 +0900

    feat: followingモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_articles, through: :favorites, source: :article
+  has_many :active_followings, dependent: :destroy, class_name: 'Following', foreign_key: 'following_user_id', inverse_of: :following_user
+  has_many :passive_followings, dependent: :destroy, class_name: 'Following', foreign_key: 'followed_user_id', inverse_of: :followed_user
+  has_many :following_users, through: :active_followings, source: :followed_user
+  has_many :followed_users, through: :passive_followings, source: :following_user
 
   validates :name, presence: true
 
