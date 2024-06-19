b21b0e4649e7d00f4d6731eb6fd431b7858c4607
kuma <kuma@sonicgarden.jp>
2024-06-19 09:49:19 +0900

    feat: フォロー・アンフォローに認可を実装

--- app/controllers/users/followings_controller.rb
+++ app/controllers/users/followings_controller.rb
@@ -1,4 +1,6 @@
 class Users::FollowingsController < Users::ApplicationController
+  before_action :authenticate_user!
+
   def create
     current_user.active_followings.find_or_create_by!(followed_user: @user)
   end
